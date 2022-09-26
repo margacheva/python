410a69419a6d96d725474fb74c29eb548ed0d95d
margacheva <miss.margacheva16@mail.ru>
2022-09-26 19:38:30 +0300

    added tests

---
+++ tests/1.py
@@ -0,0 +1,6 @@
+temp = int(input('Введите температуру:'))
+if temp > 20:
+    print('on')
+#elif temp <= 20:
+else:
+    print('off')
---
+++ tests/2.py
@@ -0,0 +1,5 @@
+t, c, v =map(int,input().split())
+if c > t and 80 >= v:
+    print('on')
+else:
+    print('off')
